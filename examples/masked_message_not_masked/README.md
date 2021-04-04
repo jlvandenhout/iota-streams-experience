06e6bf03bed1fdd714b13463e10ef33107958fcf
Jeroen van den Hout <jlvandenhout@gmail.com>
2021-04-04 02:03:31 +0200

    Add example

---
+++ examples/masked_message_not_masked/README.md
@@ -0,0 +1,9 @@
+# Duplicate announce messages
+## Issue
+When an author sends masked messages, they are not encrypted.
+
+## Actual behavior
+When the author sends a masked message, the contents do not seem to be encrypted on the tangle. This can be verified by searching for the message index and examining the data content.
+
+## Expected behavior
+The data in the masked message is expected to be encrypted, even before any keyload.
---
+++ examples/masked_message_not_masked/src/main.rs
@@ -0,0 +1,45 @@
+use iota_streams::{
+    app::transport::tangle::{
+        PAYLOAD_BYTES,
+        client::Client,
+    },
+    app_channels::api::tangle::{
+        Author,
+        Bytes,
+    },
+};
+
+#[tokio::main]
+async fn main() {
+    let encoding = "utf-8";
+    let multi_branching = false;
+
+    let author_seed = utils::random_seed();
+    let mut author = Author::new(
+        author_seed.as_str(),
+        encoding,
+        PAYLOAD_BYTES,
+        multi_branching,
+        Client::new_from_url("https://api.lb-0.testnet.chrysalis2.com"),
+    );
+
+    let announcement = author.send_announce().await.unwrap();
+
+    let public_payload = Bytes("PUBLIC MESSAGE".as_bytes().to_vec());
+    let masked_payload = Bytes("MASKED MESSAGE".as_bytes().to_vec());
+    let packet = author
+        .send_signed_packet(&announcement, &public_payload, &masked_payload)
+        .await
+        .unwrap();
+    println!("Message Index: {}", utils::get_hash(&packet.0).unwrap());
+
+    let keyload = author.send_keyload_for_everyone(&announcement).await.unwrap();
+
+    let public_payload = Bytes("PUBLIC MESSAGE".as_bytes().to_vec());
+    let masked_payload = Bytes("MASKED MESSAGE".as_bytes().to_vec());
+    let packet = author
+        .send_signed_packet(&keyload.0, &public_payload, &masked_payload)
+        .await
+        .unwrap();
+    println!("Message Index: {}", utils::get_hash(&packet.0).unwrap());
+}
